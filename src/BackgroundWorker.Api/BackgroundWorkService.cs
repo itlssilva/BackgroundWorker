96605f5e54988f2238de85864fc1e49704c37abf
Italo Silva <italo.santosilva@gmail.com>
2025-06-04 00:01:01 -0300

    Refactory

--- src/BackgroundWorker.Api/BackgroundWorkService.cs
+++ src/BackgroundWorker.Api/BackgroundWorkService.cs
@@ -1,29 +1,21 @@
-using System.Threading.Tasks;
 using BackgroundWorker.Api.Kafka;
 using BackgroundWorker.Api.MqQueueReader;
 
 namespace BackgroundWorker.Api;
 
-public class BackgroundWorkerService : BackgroundService
+public class BackgroundWorkerService(IQueueReader queueReader, IConsumerService consumerService) : BackgroundService
 {
     // private Timer _timer;
     // private int cont;
-    private readonly IQueueReader _queueReader;
-    private readonly IConsumerService _consumerService;
+    private readonly IQueueReader _queueReader = queueReader;
 
-    public BackgroundWorkerService(IQueueReader queueReader, IConsumerService consumerService)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _queueReader = queueReader;
-        _consumerService = consumerService;
-    }
-
-    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-         while (!stoppingToken.IsCancellationRequested)
+        while (!stoppingToken.IsCancellationRequested)
         {
             await DoWork(stoppingToken);
             await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
-        }  
+        }
     }
 
     private async Task DoWork(CancellationToken stoppingToken)
@@ -34,16 +26,16 @@ public class BackgroundWorkerService : BackgroundService
         // cont++;
         // var ret = _queueReader.Dequque("");
         // var ret = _queueReader.Peek();
-        var ret = _consumerService.GetMessageAsync(stoppingToken);
+        string ret = consumerService.GetMessageAsync(stoppingToken);
         if (ret != null)
             Console.WriteLine(ret);
-            // Console.WriteLine(ret.Data.Trim());
+        // Console.WriteLine(ret.Data.Trim());
         //Console.WriteLine(ret?.Data);
     }
 
     public override Task StopAsync(CancellationToken cancellationToken)
     {
-        _consumerService.ConsumerServiceClose();
+        consumerService.ConsumerServiceClose();
         // return base.StopAsync(cancellationToken);
         return Task.CompletedTask;
     }
